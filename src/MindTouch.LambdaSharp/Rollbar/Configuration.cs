e772920c483c1d76d073fbfa10646e1c8eb78588
Steve G. Bjorg <steve.bjorg@gmail.com>
2018-09-27 10:36:35 -0700

    inlined all rollbar builders

--- src/MindTouch.LambdaSharp/Rollbar/Configuration.cs
+++ src/MindTouch.LambdaSharp/Rollbar/Configuration.cs
@@ -21,16 +21,22 @@
 
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Security.Cryptography;
 using System.Text;
-using MindTouch.Rollbar.Builders;
 using MindTouch.Rollbar.Data;
 using Newtonsoft.Json;
 
 namespace MindTouch.Rollbar {
 
     public class RollbarReporter {
+
+        //--- Constants ---
+        private const int MAX_TITLE_LENGTH = 255;
+        private const string LANGUAGE = "csharp";
 
         //--- Class Fields ---
         private static readonly HashAlgorithm _algorithm = SHA1.Create();
@@ -73,6 +79,19 @@ namespace MindTouch.Rollbar {
             return sb.ToString();
         }
 
+        private IEnumerable<Exception> FlattenExceptions(Exception exception) {
+            return Enumerate().ToArray();
+
+            // local functions
+            IEnumerable<Exception> Enumerate() {
+                var current = exception;
+                do {
+                    yield return current;
+                    current = current.InnerException;
+                } while(current != null);
+            }
+        }
+
         //--- Fields ---
         private readonly string _moduleName;
         private readonly string _environment;
@@ -80,9 +99,7 @@ namespace MindTouch.Rollbar {
         private readonly string _gitSha;
         private readonly string _platform;
         private readonly JsonSerializerSettings _settings;
-        private readonly ExceptionInfoBuilder _exceptionBuilder;
-        private readonly FrameCollectionBuilder _frameBuilder;
-        private readonly TitleBuilder _titleBuilder;
+        private readonly Server _server;
 
         //--- Constructors ---
         public RollbarReporter(
@@ -92,10 +109,7 @@ namespace MindTouch.Rollbar {
             string framework,
             string gitSha
         ) {
-            if(string.IsNullOrWhiteSpace(moduleName)) {
-                throw new ArgumentNullException(nameof(moduleName));
-            }
-            _moduleName = moduleName;
+            _moduleName = moduleName ?? throw new ArgumentNullException(nameof(moduleName));
             _environment = environment ?? throw new ArgumentNullException(nameof(environment));
             _platform = $"AWS Lambda ({System.Environment.OSVersion})";
             _framework = framework;
@@ -105,25 +119,21 @@ namespace MindTouch.Rollbar {
                 NullValueHandling = NullValueHandling.Ignore
             };
             _settings.Converters.Add(new NameValueCollectionConverter());
-            _frameBuilder = new FrameCollectionBuilder();
-            _exceptionBuilder = new ExceptionInfoBuilder();
-            _titleBuilder = new TitleBuilder();
+            var assembly = Assembly.GetExecutingAssembly();
+            var host = Environment.MachineName;
+            var root = Path.GetDirectoryName(assembly.GetName().CodeBase);
+            _server = new Server(host, root, branch: null, codeVersion: null);
         }
 
         //--- Properties ---
 
         // TODO: convert `AccessToken` to `ModuleName`
         public string AccessToken =>  _moduleName;
-        public string Environment => _environment;
-        public string Platform  => _platform;
-        public string Language => "csharp";
-        public string Framework => _framework;
-        public string GitSha => _gitSha;
 
         //--- Methods ---
         public Payload CreateFromException(Exception exception, string description, string level) {
             Body body;
-            var exceptions = exception.FlattenHierarchy();
+            var exceptions = FlattenExceptions(exception);
             var traces = new List<Trace>();
             traces.Add(CreateTraceFromException(exceptions.First(), description));
             traces.AddRange(exceptions.Skip(1).Select(ex => CreateTraceFromException(ex, description: null)));
@@ -150,26 +160,99 @@ namespace MindTouch.Rollbar {
 
         private RollbarData CreateFromBody(Body body, string fingerprint, string level) {
             var timestamp = Convert.ToInt64((DateTime.UtcNow - _epoch).TotalSeconds);
-            var title = _titleBuilder.CreateFromBody(body);
+            var title = CreateTitleFromBody(body);
             return new RollbarData(
-                Environment,
+                _environment,
                 body,
                 level,
                 timestamp,
-                GitSha,
-                Platform,
-                Language,
-                Framework,
+                _gitSha,
+                _platform,
+                LANGUAGE,
+                _framework,
                 fingerprint,
                 title,
-                ServerBuilder.Instance.Server
+                _server
             );
         }
 
         private Trace CreateTraceFromException(Exception exception, string description) {
-            var ex = _exceptionBuilder.CreateFromException(exception, description);
-            var frames = _frameBuilder.CreateFromException(exception);
+            var ex = new ExceptionInfo(
+                exception.GetType().Name,
+                exception.Message,
+                string.IsNullOrWhiteSpace(description)
+
+                    // use the exception message when no custom description is provided
+                    ? exception.ToString()
+
+                    // combine the custom description with the exception message
+                    : $"{description}{System.Environment.NewLine}{exception}"
+            );
+            var frames = CreateFrameCollectionFromException(exception);
             return new Trace(ex, frames);
+        }
+
+        private string CreateTitleFromBody(Body body) {
+            string title;
+            if(body.Message != null) {
+                title = body.Message.Body;
+            } else {
+                var trace = body.Trace ?? body.TraceChain.LastOrDefault() ?? body.TraceChain.Last();
+                title = $"{trace.Exception.ClassName}: {trace.Exception.Message}";
+            }
+            return (title.Length > MAX_TITLE_LENGTH) ? title.Substring(0, MAX_TITLE_LENGTH) : title;
+        }
+
+        private IEnumerable<Frame> CreateFrameCollectionFromException(Exception exception) {
+            var stackTrace = new System.Diagnostics.StackTrace(exception, true);
+            var lines = new List<Frame>();
+            var stackFrames = stackTrace.GetFrames();
+            if(stackFrames == null || stackFrames.Length == 0) {
+                return lines;
+            }
+
+            // process all stack frames
+            foreach(var frame in stackFrames) {
+                var lineNumber = frame.GetFileLineNumber();
+                var fileName = frame.GetFileName();
+
+                string methodName = null;
+                var method = frame.GetMethod();
+                if(method != null) {
+                    var methodParams = method.GetParameters();
+
+                    // add method parameters to the method name. helpful for resolving overloads.
+                    methodName = method.Name;
+                    if(methodParams.Length > 0) {
+                        var paramDesc = string.Join(", ", methodParams.Select(p => p.ParameterType + " " + p.Name));
+                        methodName = methodName + "(" + paramDesc + ")";
+                    }
+                }
+
+                // when the line number is zero, you can try using the IL offset
+                if(lineNumber == 0) {
+                    lineNumber = frame.GetILOffset();
+                }
+
+                if(lineNumber == -1) {
+                    lineNumber = frame.GetNativeOffset();
+                }
+
+                // line numbers less than 0 are not accepted
+                if(lineNumber < 0) {
+                    lineNumber = 0;
+                }
+
+                // file names aren't always available, so use the type name instead, if possible
+                if(string.IsNullOrEmpty(fileName)) {
+                    fileName = method.ReflectedType.ToString();
+                }
+
+                // NOTE: Set CodeContext and Code (lines of code above and below the line that raised the exception).
+                lines.Add(new Frame(fileName, lineNumber, null, methodName));
+            }
+
+            return lines.ToArray();
         }
     }
 }
