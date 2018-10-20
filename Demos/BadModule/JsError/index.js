d4d8a4dcc084adb32ee974407c72338b2041b5a1
Steve G. Bjorg <steve.bjorg@gmail.com>
2018-10-19 19:16:18 -0700

    added bad JS function

---
+++ Demos/BadModule/JsError/index.js
@@ -0,0 +1,15 @@
+class CustomError extends Error {
+    constructor(message) {
+        this.name = "CustomError";
+        this.message = message;
+    }
+}
+
+async function badFunction() {
+    throw new CustomError("Oops!... I did it again");
+}
+
+exports.handler = async (event, context, callback) => {
+    return await badFunction();
+}
+
