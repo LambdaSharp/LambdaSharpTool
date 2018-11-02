3fc93ad8f42b71308a968def21423c65a8671a7f
Steve G. Bjorg <steve.bjorg@gmail.com>
2018-11-02 12:08:29 -0700

    script for installing global dotnet tool from local build

---
+++ src/MindTouch.LambdaSharp.Tool/install-cli.sh
@@ -0,0 +1,18 @@
+rm *.nupkg
+
+dotnet publish \
+    --configuration Release
+
+dotnet pack \
+    --configuration Release \
+    --output ./
+
+dotnet tool uninstall \
+    --global \
+    MindTouch.LambdaSharp.Tool \
+
+dotnet tool install \
+    --global \
+    --add-source $LAMBDASHARP/src/MindTouch.LambdaSharp.Tool/ \
+    MindTouch.LambdaSharp.Tool \
+    --version 0.4.0.1-RC1
