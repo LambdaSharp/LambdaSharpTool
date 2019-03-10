ad54e24f8046742d4d97e5c09e8fab1767a66901
Steve G. Bjorg <steve.bjorg@gmail.com>
2019-03-09 22:06:33 -0800

    dispatch table class and tests

---
+++ Tests/Tests.LambdaSharp/Internal/APIGatewayDispatchTableTests.cs
@@ -0,0 +1,149 @@
+﻿/*
+ * MindTouch λ#
+ * Copyright (C) 2018-2019 MindTouch, Inc.
+ * www.mindtouch.com  oss@mindtouch.com
+ *
+ * For community documentation and downloads visit mindtouch.com;
+ * please review the licensing section.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Threading.Tasks;
+using Amazon.Lambda.APIGatewayEvents;
+using FluentAssertions;
+using LambdaSharp.Internal;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Tests.LambdaSharp.Internal {
+
+    public class APIGatewayDispatchTableTests {
+
+        //--- Types ---
+        public class SimpleRequest {
+
+            //--- Properties ---
+            public string Text { get; set; }
+
+        }
+
+        public class SimpleResponse {
+
+            //--- Properties ---
+            public string Message { get; set; }
+        }
+
+        //--- Class Fields ---
+        private static SimpleResponse DefaultResponse = new SimpleResponse {
+            Message = "I was here!"
+        };
+
+        //--- Class Methods ---
+        private static string SerializeJson(object value) => JsonConvert.SerializeObject(value);
+
+        private static SimpleResponse CreateResponse(object value) => new SimpleResponse {
+            Message = $"Value: {value}"
+        };
+
+        //--- Methods ---
+        [Fact]
+        public void InvokeMethodNoParametersWithSimpleResponseSync() {
+            Test(
+                nameof(MethodNoParametersWithSimpleResponseSync),
+                new APIGatewayProxyRequest(),
+                new APIGatewayProxyResponse {
+                    Body = SerializeJson(DefaultResponse),
+                    StatusCode = 200
+                }
+            ).Wait();
+        }
+
+        [Fact]
+        public void InvokeMethodNoParametersWithAPIGatewayProxyResponseSync() {
+            Test(
+                nameof(MethodNoParametersWithAPIGatewayProxyResponseSync),
+                new APIGatewayProxyRequest(),
+                new APIGatewayProxyResponse {
+                    Body = SerializeJson(DefaultResponse),
+                    StatusCode = 200
+                }
+            ).Wait();
+        }
+
+        [Fact]
+        public void InvokeMethodNoParametersWithSimpleResponseAsync() {
+            Test(
+                nameof(MethodNoParametersWithSimpleResponseAsync),
+                new APIGatewayProxyRequest(),
+                new APIGatewayProxyResponse {
+                    Body = SerializeJson(DefaultResponse),
+                    StatusCode = 200
+                }
+            ).Wait();
+        }
+
+        [Fact]
+        public void InvokeMethodNoParametersWithAPIGatewayProxyResponseAsync() {
+            Test(
+                nameof(MethodNoParametersWithAPIGatewayProxyResponseAsync),
+                new APIGatewayProxyRequest(),
+                new APIGatewayProxyResponse {
+                    Body = SerializeJson(DefaultResponse),
+                    StatusCode = 200
+                }
+            ).Wait();
+        }
+
+        public SimpleResponse MethodNoParametersWithSimpleResponseSync() => DefaultResponse;
+
+        public APIGatewayProxyResponse MethodNoParametersWithAPIGatewayProxyResponseSync()
+            => new APIGatewayProxyResponse {
+                Body = SerializeJson(DefaultResponse),
+                StatusCode = 200
+            };
+
+        public async Task<SimpleResponse> MethodNoParametersWithSimpleResponseAsync() {
+            await Task.Delay(TimeSpan.FromMilliseconds(1));
+            return DefaultResponse;
+        }
+
+        public async Task<APIGatewayProxyResponse> MethodNoParametersWithAPIGatewayProxyResponseAsync() {
+            await Task.Delay(TimeSpan.FromMilliseconds(1));
+            return new APIGatewayProxyResponse {
+                Body = SerializeJson(DefaultResponse),
+                StatusCode = 200
+            };
+        }
+
+        private async Task Test(string methodName, APIGatewayProxyRequest request, APIGatewayProxyResponse expectedResponse) {
+
+            // Arrange
+            var dispatchTable = new APIGatewayDispatchTable(GetType());
+            dispatchTable.Add("test", methodName);
+
+            // Act
+            var found = dispatchTable.TryGetDispatcher("test", out var dispatcher);
+
+            // Assert
+            found.Should().Be(true);
+
+            // Act
+            var response = await dispatcher(this, request);
+
+            // Assert
+            response.Should().BeEquivalentTo(expectedResponse);
+        }
+    }
+}
---
+++ src/LambdaSharp/Internal/APIGatewayDispatchTable.cs
@@ -0,0 +1,133 @@
+/*
+ * MindTouch λ#
+ * Copyright (C) 2018-2019 MindTouch, Inc.
+ * www.mindtouch.com  oss@mindtouch.com
+ *
+ * For community documentation and downloads visit mindtouch.com;
+ * please review the licensing section.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Amazon.Lambda.APIGatewayEvents;
+using Newtonsoft.Json;
+
+namespace LambdaSharp.Internal {
+
+    public class APIGatewayDispatchTable {
+
+        //--- Types ---
+        public delegate Task<APIGatewayProxyResponse> Dispatcher(object target, APIGatewayProxyRequest request);
+
+        //--- Class Methods ---
+        private static string SerializeJson(object value) => JsonConvert.SerializeObject(value);
+
+        // TODO (2019-03-09, bjorg): we should use the Lambda serializer, but there is no-non generic version available
+        private static object DeserializeJson(string json, Type type) => JsonConvert.DeserializeObject(json, type);
+
+        private static Func<APIGatewayProxyRequest, object> CreateParameterResolver(ParameterInfo parameter) {
+            Func<APIGatewayProxyRequest, object> resolver;
+
+            // check if parameter represents the body of the request
+            if(parameter.Name == "request") {
+                resolver = request => DeserializeJson(request.Body, parameter.ParameterType);
+            } else {
+
+                // TODO (2019-03-09, bjorg): respect default argument value when defined
+
+                // create function for getting default parameter value
+                Func<object> getDefaultValue;
+                if(parameter.ParameterType.IsValueType) {
+                    getDefaultValue = () => Activator.CreateInstance(parameter.ParameterType);
+                } else {
+                    getDefaultValue = () => null;
+                }
+
+                // create function to resolve parameter
+                resolver = request => {
+
+                    // attempt to resolve the parameter from stage variables, path parameters, and query parameters
+                    var success = request.StageVariables.TryGetValue(parameter.Name, out var value)
+                        || request.PathParameters.TryGetValue(parameter.Name, out value)
+                        || request.QueryStringParameters.TryGetValue(parameter.Name, out value);
+
+                    // if resolved, return the converted value; otherwise the default value
+                    return success
+                        ? Convert.ChangeType(value, Nullable.GetUnderlyingType(parameter.ParameterType) ?? parameter.ParameterType)
+                        : getDefaultValue();
+                };
+            }
+            return resolver;
+        }
+
+        private static Dispatcher CreateMethodDelegate(MethodInfo method) {
+
+            // create function to resolve all method parameters
+            var resolvers = method.GetParameters().Select(CreateParameterResolver).ToArray();
+
+            // create adapter to invoke custom method
+            Dispatcher methodAdapter;
+            if(method.ReturnType == typeof(Task<APIGatewayProxyResponse>)) {
+                methodAdapter = (object target, APIGatewayProxyRequest request) => {
+                    return (Task<APIGatewayProxyResponse>)method.Invoke(target, resolvers.Select(fill => fill(request)).ToArray());
+                };
+            } else if(method.ReturnType == typeof(APIGatewayProxyResponse)) {
+                methodAdapter = async (object target, APIGatewayProxyRequest request) => {
+                    return (APIGatewayProxyResponse)method.Invoke(target, resolvers.Select(fill => fill(request)).ToArray());
+                };
+            } else if(method.ReturnType.IsGenericType && method.ReturnType.GetGenericTypeDefinition() == typeof(Task<>)) {
+                var resolveReturnValue = method.ReturnType.GetProperty("Result");
+                methodAdapter = async (object target, APIGatewayProxyRequest request) => {
+                    var task = (Task)method.Invoke(target, resolvers.Select(fill => fill(request)).ToArray());
+                    await task;
+                    var result = resolveReturnValue.GetValue(task);
+                    return new APIGatewayProxyResponse {
+                        StatusCode = 200,
+                        Body = SerializeJson(result)
+                    };
+                };
+            } else if((method.ReturnType == typeof(Task)) || (method.ReturnType == typeof(void))) {
+
+                // TODO (2019-03-09, bjorg): something to consider for the future
+                throw new NotSupportedException("void return type is not supported");
+            } else {
+                methodAdapter = async (object target, APIGatewayProxyRequest request) => {
+                    var result = method.Invoke(target, resolvers.Select(fill => fill(request)).ToArray());
+                    return new APIGatewayProxyResponse {
+                        StatusCode = 200,
+                        Body = SerializeJson(result)
+                    };
+                };
+            }
+            return methodAdapter;
+        }
+
+        //--- Fields ---
+        private readonly Type _targetType;
+        private readonly Dictionary<string, Dispatcher> _mappings = new Dictionary<string, Dispatcher>();
+
+        //--- Constructors ---
+        public APIGatewayDispatchTable(Type targetType) {
+            _targetType = targetType ?? throw new ArgumentNullException(nameof(targetType));
+        }
+
+        //--- Methods ---
+        public void Add(string key, string methodName) => _mappings.Add(key, CreateMethodDelegate(_targetType.GetMethod(methodName)));
+        public bool TryGetDispatcher(string key, out Dispatcher dispatcher) => _mappings.TryGetValue(key, out dispatcher);
+    }
+}
