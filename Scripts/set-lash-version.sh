6e2ef753fd89de580375dd4e343ad1d83fe06ad5
Steve Bjorg <steve.bjorg@gmail.com>
2020-05-06 15:09:34 -0700

    rc1 version bump

--- Scripts/set-lash-version.sh
+++ Scripts/set-lash-version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export LAMBDASHARP_VERSION_PREFIX=0.8.0.0
-export LAMBDASHARP_VERSION_SUFFIX=
+export LAMBDASHARP_VERSION_SUFFIX=rc1
 
 # create full version text
 if [ -z "$LAMBDASHARP_VERSION_SUFFIX" ]; then
