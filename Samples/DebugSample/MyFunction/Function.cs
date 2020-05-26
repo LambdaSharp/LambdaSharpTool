eb4860cb2fe2d38b3cc45b7916e82f459431f2e8
Steve Bjorg <steve.bjorg@gmail.com>
2020-05-26 14:30:32 -0700

    added code comment

--- Samples/DebugSample/MyFunction/Function.cs
+++ Samples/DebugSample/MyFunction/Function.cs
@@ -35,6 +35,8 @@ namespace Sample.Debug.MyFunction {
 
             // LogDebug() is only emitted to the logs when the DEBUG_LOGGING_ENABLED environment variable is set
             LogDebug("this will only show if DEBUG_LOGGING_ENABLED environment variable is set");
+
+            // to avoid unnecessary overhead, check if debug logging is enabled before constructing debug output
             if(Provider.DebugLoggingEnabled) {
                 LogDebug("more complex statements should be guarded using the Provider.DebugLoggingEnabled property");
             }
